968b75e0f94100a6c2a0cd579de0147b6aabfc0d
oivv-platform <naviddehghani@live.com>
2024-06-25 01:58:39 +0200

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,12 +1,1 @@
-window.addEventListener('scroll', function() {
-    const header = document.querySelector('header');
-    if (window.scrollY > 50) {
-        header.classList.add('scrolled');
-    } else {
-        header.classList.remove('scrolled');
-    }
-});
 
-document.querySelector('.menu-toggle').addEventListener('click', function() {
-    document.querySelector('nav ul').classList.toggle('active');
-});
